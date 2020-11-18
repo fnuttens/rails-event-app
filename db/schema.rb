77b60489b41a7b2eec25f56cb2779d40875bc67d
NUTTENS Florent <florent.nuttens@e-i.com>
2020-11-18 10:24:23 +0100

    feat: add seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_160508) do
+ActiveRecord::Schema.define(version: 2020_11_18_091617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_160508) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.text "decription"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Event.destroy_all
+UserToEvent.destroy_all
+
+100.times do
+    User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+20.times do
+    Event.create(name: Faker::Music.band, address: Faker::Address.street_address, description: Faker::Lorem.sentences(number: 3))
+end
